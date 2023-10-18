a96390be51c0d414ad2227ebea8d8a2f8afd8381
Sundar Jayabose <sundarjayabose@Sundars-MacBook-Pro.local>
2023-10-18 14:06:15 -0400

    feat: setup socket.io server and basic events
    events added: connect, signal, newProducer, consumerCatchUp, consume with console.log stmts
    
    Co-authored-by: Tyler Frye <tyler.frye42@gmail.com>
    Co-authored-by: Stephen Watzman <Ixisixi@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,39 @@
 import express from 'express';
+import cors from 'cors';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 const app = express();
 
-app.get('/', (_, res) => {
-  res.send('Hello, TypeScript Express Server!');
+const httpServer = createServer(app);
+const ioServer = new Server(httpServer, { 
+  cors: { origin: '*' }
 });
 
-export default app;
+app.use(cors());
+
+ioServer.on('connection', (socket) => {
+  console.log('ioServer connection')
+
+  socket.on('connect', () => {
+    console.log('ioServer:  connect event received.');
+  });
+
+  socket.on('signal', () => {
+    console.log('ioServer:  signal event received.');
+  });
+
+  socket.on('newProducer', () => {
+    console.log('ioServer:  newProducer event received.');
+  });
+
+  socket.on('consumerCatchUp', () => {
+    console.log('ioServer:  consumerCatchUp event received.');
+  });
+
+  socket.on('consume', () => {
+    console.log('ioServer:  consume event received.');
+  });
+});
+
+export default httpServer;
