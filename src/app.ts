b7dc69ca481ceba83ea0bbc86887ab0cea0d9ca2
frye-t <tyler.frye42@gmail.com>
2023-10-25 14:15:30 -0400

    refactor: refactored socket event handlers to their own global functions
    Co-authored-by: Stephen Watzman <ixisixi@gmail.com>
    Co-authored-by: Jay Jayabose <jayabose@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -42,76 +42,86 @@ const findUUIDBySocket = (targetSocket: Socket): string | undefined => {
   return undefined;
 };
 
-/* eslint-disable max-lines-per-function */
+const handleClientConnect = (socket: Socket, data: ClientConnectProps): void => {
+  const { type, id } = data;
+  if (type === 'sfu') {
+    sfuSocket = socket;
+    console.log('SFU Connected, Storing Socket');
+  } else if (type === 'client') {
+    if (!sfuSocket) {
+      socket.emit('error', 'SFU not connected, try again later');
+      return;
+    }
+    console.log('Client Connected', id);
+    clients.set(id, socket);
+  } else {
+    socket.emit('error', 'Missing valid type property');
+  }
+};
+
+const handleProducerHandshake = (socket: Socket, data: ProducerHandshakeProps): void => {
+  if (socket === sfuSocket) {
+    const client = clients.get(data.clientId);
+    if (client) {
+      console.log('Sending data to Client from SFU:', data.clientId);
+      client.emit('producerHandshake', data);
+    }
+  } else {
+    console.log('Sending data to SFU from:', data.clientId);
+    sfuSocket.emit('producerHandshake', data);
+  }
+};
+
+const handleConsumerHandshake = (socket: Socket, data: ConsumerHandshakeProps): void => {
+  if (socket === sfuSocket) {
+    const client = clients.get(data.clientId);
+    if (client) {
+      console.log('Sending consumer data to Client from SFU:', data.clientId);
+      client.emit('consumerHandshake', data);
+    } else {
+      console.log('Client not found in the clients Map.');
+    }
+  } else {
+    console.log('Sending consumer data to SFU from:', data.clientId);
+    sfuSocket.emit('consumerHandshake', data);
+  }
+};
+
+const handleClientDisconnect = (socket: Socket): void => {
+  console.log('client disconnected');
+  if (socket === sfuSocket) {
+    return; // Handle SFU Disconnect
+  }
+
+  const clientUUID = findUUIDBySocket(socket);
+  if (clientUUID) {
+    clients.delete(clientUUID);
+    for (const [_uuid, sc] of clients) {
+      sc.emit('clientDisconnect', { clientId: clientUUID });
+    }
+  } else {
+    console.log('Socket not found in the clients Map.');
+  }
+};
+
 ioServer.on('connection', (socket: Socket) => {
   console.log('ioServer connection');
+
   socket.on('clientConnect', (data: ClientConnectProps) => {
-    const { type, id } = data;
-    if (type === 'sfu') {
-      sfuSocket = socket;
-      console.log('SFU Connected, Storing Socket');
-    } else if (type === 'client') {
-      if (!sfuSocket) {
-        socket.emit('error', 'SFU not connected, try again later');
-        return;
-      }
-      console.log('Client Connected', id);
-      clients.set(id, socket);
-    } else {
-      socket.emit('error', 'Missing valid type property');
-    }
+    handleClientConnect(socket, data);
   });
 
   socket.on('producerHandshake', (data: ProducerHandshakeProps) => {
-    // console.log("got a producer handshake signal")
-    if (socket === sfuSocket) {
-      const client = clients.get(data.clientId);
-      if (client) {
-        console.log('Sending data to Client from SFU:', data.clientId);
-        client.emit('producerHandshake', data);
-      }
-    } else {
-      console.log('Sending data to SFU from:', data.clientId);
-      sfuSocket.emit('producerHandshake', data);
-    }
+    handleProducerHandshake(socket, data);
   });
 
   socket.on('consumerHandshake', (data: ConsumerHandshakeProps) => {
-    if (socket === sfuSocket) {
-      const client = clients.get(data.clientId);
-      if (client) {
-        console.log('Sending consumer data to Client from SFU:', data.clientId);
-        client.emit('consumerHandshake', data);
-      }
-    } else {
-      console.log('Sending consumer data to SFU from:', data.clientId);
-      sfuSocket.emit('consumerHandshake', data);
-    }
+    handleConsumerHandshake(socket, data);
   });
 
   socket.on('disconnect', () => {
-    console.log('client disconnected');
-
-    // TODO: Handle SFU Disconnect
-    if (socket === sfuSocket) {
-      return;
-    }
-
-    const clientUUID = findUUIDBySocket(socket);
-    if (clientUUID) {
-      clients.delete(clientUUID);
-
-      // ESLint complians about this variable name, but need it to allow TS to compile
-      for (const [_uuid, sc] of clients) {
-        sc.emit('clientDisconnect', { clientId: clientUUID });
-      }
-    } else {
-      console.log('Socket not found in the clients Map.');
-    }
+    handleClientDisconnect(socket);
   });
 });
 
 export default httpServer;
-// 600e7cfe-7086-42bb-9585-4b815f2938b8
-
-// 0c494178-74ac-4909-85d7-7129a87755b4
